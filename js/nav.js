476e38bcb1fe2cf9c89c1fe281d466a3cb9d2dc5
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2024-03-10 14:51:42 +0100

    setting up react for merge

--- js/nav.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', function() {
-    this.classList.toggle('clicked');
-  });
-  
-// Toggle .menu when .hamburger is clicked
-document.querySelector('.hamburger').addEventListener('click', function() {
-    document.querySelector('.menu').classList.toggle('show');
-  });
-
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => (
+  <div className="page">
+    <Header />
+    <Outlet />
+    <Footer />
+  </div>
+);
+
+export default Layout;
